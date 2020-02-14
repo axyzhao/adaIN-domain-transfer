3ffec90794096db18e5f202d12860de389c55bf8
Alex Zhao <axyzhao@berkeley.edu>
2020-02-14 12:57:25 -0800

    update classifier

--- classify.py
+++ classify.py
@@ -4,7 +4,6 @@ import datetime
 import matplotlib.pyplot as plt
 from torch import optim
 import numpy as np
-import resnet
 from torchvision import datasets, transforms
 from pathlib import Path
 
@@ -23,6 +22,7 @@ train_files = glob.glob('data/PACS/*train.hdf5')
 test_files = glob.glob('data/PACS/*test.hdf5')
 target_domain = 'data/PACS/cartoon_train.hdf5'
 
+eval_losses = []
 
 # separate batch functions for image and labels, since we need to apply a transformation to images
 def batch_img(iterable, batch_size=32):
@@ -48,16 +48,15 @@ def content_tf(img):
     img = img.astype('float')
     img = Image.fromarray(img.astype('uint8'), 'RGB')
     transform_list = transforms.Compose([
+        transforms.RandomCrop(227),
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-         #transforms.RandomCrop(256),
-        # transforms.RandomHorizontalFlip(),
-        #transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
     return transform_list(img).float()
 
 def save_img(img, name):
     img = transforms.ToTensor()(img)
-    plt.imsave(name, img.permute(1, 2, 0).numpy().astype('float')/255)
+    plt.imsave(name, img.permute(1, 2, 0).cpu().numpy().astype('float')/255)
 
 def shuffle_data(training_data, training_labels):
     idx = np.arange(len(training_data))
@@ -84,28 +83,30 @@ def style_transfer(vgg, decoder, content, style, alpha=1.0,
     return decoder(feat)
 
 def train(model, content_data, content_labels, style_data, style_labels, batch_size=32):
+    model.train()
     all_count = correct_count = 0
     l = min(len(content_data), len(style_data))
     content_img_batches, content_label_batches = (batch_img(content_data, batch_size=batch_size),
-                                            batch(style_labels, batch_size=batch_size))
+                                            batch(content_labels, batch_size=batch_size))
     style_img_batches, style_label_batches = (batch_img(style_data, batch_size=batch_size),
                                             batch(style_labels, batch_size=batch_size))
     for i in range(l // batch_size):
         content_img, content_label = (next(content_img_batches), next(content_label_batches))
+        content_img = content_img.to(device)
         style_img, style_label = (next(style_img_batches), next(style_label_batches))
-        content_label = torch.tensor(content_label).long()
-        style_label = torch.tensor(style_label).long()
+        style_img = style_img.to(device)
+        content_label = torch.tensor(content_label).long().to(device)
+        style_label = torch.tensor(style_label).long().to(device)
 
         model.optimizer.zero_grad()
        # if np.random.binomial(1, 0.5):
-       #     output = style_transfer(vgg, decoder, content_img_tf, style_img_tf,
-        #                            args.alpha)
-        #else:
-         #   output = content_img_tf
+       #     output = style_transfer(vgg, decoder, content_img, style_img, args.alpha)
+       # else:
+       #     output = content_img
 
         output = style_transfer(vgg, decoder, content_img, style_img, args.alpha)
 
-        probabilities = model.forward(output)#.squeeze()
+        probabilities = model.forward(output)
         loss = model.loss(probabilities, content_label)
         loss.backward()
         model.optimizer.step()
@@ -120,7 +121,7 @@ def train(model, content_data, content_labels, style_data, style_labels, batch_s
             save_image(style_img, str(style_name))
             
         for j in range(len(content_label)):
-            true_label = content_label.numpy()[j]
+            true_label = content_label.cpu().numpy()[j]
             pred_label = probabilities[j].argmax()
             if(true_label == pred_label):
                 correct_count += 1
@@ -129,16 +130,20 @@ def train(model, content_data, content_labels, style_data, style_labels, batch_s
     print("Number tested: {}".format(all_count))
     print("Model accuracy: {}".format(correct_count / all_count))
 
-def evaluate(model, data, labels):
+def evaluate(model, data, labels, batch_size=32):
     print("\n")
     print("Evaluating model on target domain...")
     all_count = correct_count = 0
-    for img, label in zip(data, labels):
+    img_batches, label_batches = (batch_img(data, batch_size=batch_size),
+                                            batch(labels, batch_size=batch_size))
+    l = len(data)
+    for i in range(l // batch_size):
+        img, label = (next(img_batches), next(label_batches))
         model.eval()
         with torch.no_grad():
             # create minibatch by unsqueezing
-            img = content_tf(img).unsqueeze(0).float()
-            label = torch.tensor(label).unsqueeze(0).long()
+            img = img.to(device)
+            label = torch.tensor(label).long().to(device)
             # forward image through model
             probabilities = model.forward(img)
             loss_ = model.loss(probabilities, label)
@@ -147,14 +152,16 @@ def evaluate(model, data, labels):
                 print("Loss at step {} is {}".format(all_count, loss_))
             highest = probabilities.argmax(dim=1)
             for i in range(len(label)):
-                true_label = label.numpy()[i]
+                true_label = label.cpu().numpy()[i]
                 pred_label = probabilities[i].argmax()
                 if(true_label == pred_label):
                     correct_count += 1
                 all_count += 1
     print('\n')
-    print("Number Of Images Tested =", all_count)
-    print("\nModel Accuracy =", (correct_count/all_count))
+    print("Number tested: {}".format(all_count))
+    print("Model accuracy: {}".format(correct_count / all_count))
+    eval_losses.append(correct_count/all_count)
+
 parser = argparse.ArgumentParser()
 # Basic options
 parser.add_argument('--content', type=str,
@@ -196,7 +203,7 @@ parser.add_argument(
     '--style_interpolation_weights', type=str, default='',
     help='The weight for blending the style of multiple style images')
 parser.add_argument(
-    '--batch_size', type=int, default=1,
+    '--batch_size', type=int, default=32,
     help='batch size')
 parser.add_argument(
     '--num_epochs', type=int, default=5,
@@ -225,13 +232,13 @@ model = torch.hub.load('pytorch/vision:v0.5.0', 'resnet18', pretrained=False)
 num_ftrs = model.fc.in_features
 
 model.fc = nn.Linear(num_ftrs, num_output_classes)
-model.optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=3e-4)
-# batch size = 32
-# random crop -- shift data to a different position
+model.optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=5e-4)
 model.loss = nn.CrossEntropyLoss()
+model = model.to(device)
 show_every = 100
 
 style_data, style_labels = open_file(target_domain)
+test_data, test_labels = open_file('data/PACS/cartoon_test.hdf5')
 for f in train_files:
     if f == target_domain:
         continue
@@ -245,6 +252,24 @@ for f in train_files:
         content_data, content_labels = shuffle_data(content_data, content_labels)
         style_data, style_Labels = shuffle_data(style_data, style_labels)
         train(model, content_data, content_labels, style_data, style_labels, batch_size=args.batch_size)
+    evaluate(model, test_data, test_labels, batch_size=args.batch_size)
 
-evaluate(model, style_data, style_labels)
+for f in train_files:
+    if f == target_domain:
+        continue
+    content_data, content_labels = open_file(f)
+    print("Starting training on new domains...")
+    print("Transferring from {} --> {}".format(target_domain, f))
+
+    for epoch in range(args.num_epochs):
+        print("Starting epoch {}".format(epoch))
+        # shuffle data
+        content_data, content_labels = shuffle_data(content_data, content_labels)
+        style_data, style_Labels = shuffle_data(style_data, style_labels)
+        train(model, content_data, content_labels, style_data, style_labels, batch_size=args.batch_size)
+    evaluate(model, test_data, test_labels, batch_size=args.batch_size)
+evaluate(model, test_data, test_labels, batch_size=args.batch_size)
+with open("{}_accuracies".format(args.experiment_name), 'w') as f:
+    for e in eval_losses:
+        f.write('%s\n' % e)
 torch.save(model, '{}_resnet_classifier.pt'.format(args.experiment_name))
